fb52ed00acc9061e1051a9933bc71d066af09d68
vasanth-git-handle <vasanthkvdriveacc@gmail.com>
2025-07-16 17:33:44 +0530

    Initial commit - Bail Reckoner full-stack project

---
+++ BailReckoner/src/main/java/com/example/BailReckoner/BailReckonerApplication.java
@@ -0,0 +1,13 @@
+package com.example.BailReckoner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BailReckonerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BailReckonerApplication.class, args);
+	}
+
+}
---
+++ BailReckoner/src/test/java/com/example/BailReckoner/BailReckonerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BailReckoner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BailReckonerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
